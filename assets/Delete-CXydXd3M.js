2982bccb520286782fc7f01554f366fb47ecb159
Ch-SaiTeja333 <chintakrindasaiteja@gmail.com>
2025-07-31 22:29:40 +0530

    Updates

---
+++ assets/Delete-CXydXd3M.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DovU7ccc.js";function r(){return e.jsx("div",{children:e.jsx("h1",{children:"Delete"})})}export{r as default};
---
+++ assets/Update-CnuMaGct.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DovU7ccc.js";function r(){return t.jsx("div",{children:t.jsx("h1",{children:"Update"})})}export{r as default};
